2e8f497f8cae17fa00cf9f10f7202c7daccd8cb8
RRiverWest <null>
2024-10-11 18:06:00 +0000

    add sway and rofi config

--- make_link.sh
+++ make_link.sh
@@ -4,3 +4,5 @@ ln -s ~/dotfiles/kitty ~/.config/kitty
 ln -s ~/dotfiles/nvim ~/.config/nvim 
 ln -s ~/dotfiles/alacritty ~/.config/alacritty
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/sway ~/.config/sway
+ln -s ~/dotfiles/rofi ~/.config/rofi
