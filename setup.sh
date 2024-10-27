761052459babdd3d5a3021074b72245ef3d15f02
RRiverWest <null>
2024-10-27 21:44:39 +0900

    remove swaylock

--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 ln -s ~/dotfiles/kitty ~/.config/kitty
-ln -s ~/dotfiles/nvim ~/.config/nvim 
+# ln -s ~/dotfiles/nvim ~/.config/nvim 
 ln -s ~/dotfiles/alacritty ~/.config/alacritty
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/sway ~/.config/sway
 ln -s ~/dotfiles/rofi ~/.config/rofi
+
+# neovim setup
+git clone https://github.com/RRiverWest/lazy-nvim-dotfil ~/.config/nvim/
+https://github.com/RRiverWest/lazy-nvim-dotfile
 
 # make screenshot image directory
 mkdir ~/Images
