28869d983802b7bc367b49cc403a06bcea84739b
RRiverWest <kawa.ryo.0126@gmail.com>
2025-07-04 20:26:05 +0900

    add: zsh-autocomplete and delete zsh-autosuggestions

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -3,6 +3,6 @@
 # oh-my-zsh install 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/marlonrichert/zsh-autocomplete ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
