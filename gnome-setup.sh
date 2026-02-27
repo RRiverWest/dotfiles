5117de28a9d39094e7c85ee6c4f5013a442ed196
RRiverWest <147599678+RRiverWest@users.noreply.github.com>
2026-02-27 11:05:34 +0900

    feat: add gnome setting

---
+++ gnome-setup.sh
@@ -0,0 +1,43 @@
+gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
+
+gsettings set org.gnome.shell.keybindings focus-active-notification "[]"
+gsettings set org.gnome.shell.keybindings open-new-window-application-1 "[]"
+gsettings set org.gnome.shell.keybindings open-new-window-application-2 "[]"
+gsettings set org.gnome.shell.keybindings open-new-window-application-3 "[]"
+gsettings set org.gnome.shell.keybindings open-new-window-application-4 "[]"
+gsettings set org.gnome.shell.keybindings open-new-window-application-5 "[]"
+gsettings set org.gnome.shell.keybindings open-new-window-application-6 "[]"
+gsettings set org.gnome.shell.keybindings open-new-window-application-7 "[]"
+gsettings set org.gnome.shell.keybindings open-new-window-application-8 "[]"
+gsettings set org.gnome.shell.keybindings open-new-window-application-9 "[]"
+
+gsettings set org.gnome.shell.keybindings toggle-application-view "[]"
+
+gsettings set org.gnome.desktop.wm.keybindings begin-move "[]"
+gsettings set org.gnome.desktop.wm.keybindings begin-resize "[]"
+gsettings set org.gnome.desktop.wm.keybindings cycle-group "[]"
+gsettings set org.gnome.desktop.wm.keybindings cycle-group-backward "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications "['<Super>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-last "[]"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-last "[]"
+gsettings set org.gnome.desktop.wm.keybindings panel-run-dialog "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "[]"
+gsettings set org.gnome.desktop.wm.keybindings cycle-panels "[]"
+gsettings set org.gnome.desktop.wm.keybindings cycle-panels-backward "[]"
+gsettings set org.gnome.desktop.wm.keybindings cycle-windows "[]"
+gsettings set org.gnome.desktop.wm.keybindings cycle-windows-backward "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-group "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-group-backward "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-panels "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-panels-backward "[]"
+
+gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-automatic false
+gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-to 0 
+gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-from 0
+
+
