cf2b3f81b54f80cbad07819501472bf0a0b5f757
RRiverWest <147599678+RRiverWest@users.noreply.github.com>
2026-06-18 23:28:05 +0900

    feat: add zed setting

--- setup.sh
+++ setup.sh
@@ -5,11 +5,12 @@ ln -s ~/dotfiles/kitty ~/.config/kitty
 # ln -s ~/dotfiles/alacritty ~/.config/alacritty
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/sway ~/.config/sway
-ln -s ~/dotfiles/rofi ~/.config/rofi
-ln -s ~/dotfiles/i3 ~/.config/i3
-ln -s ~/dotfiles/xfce4 ~/.config/xfce4 
+# ln -s ~/dotfiles/rofi ~/.config/rofi
+# ln -s ~/dotfiles/i3 ~/.config/i3
+# ln -s ~/dotfiles/xfce4 ~/.config/xfce4 
 ln -s ~/dotfiles/superfile ~/.config/superfile
 ln -s ~/dotfiles/fuzzel ~/.config/fuzzel
+ln -s ~/dotfiles/zed ~/.config/zed
 
 # neovim setup
 # git clone https://github.com/RRiverWest/lazy-nvim-dotfil ~/.config/nvim/
